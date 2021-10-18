2b75493d7ceda556cf8b2a7b4a516cff243de2da
Tim Clephas <tim.clephas@nobleo.nl>
2021-10-18 19:36:10 +0200

    Nieuwe erven

---
+++ launch/simulation.launch.py
@@ -0,0 +1,34 @@
+import os
+import pathlib
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions.path_join_substitution import PathJoinSubstitution
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import launch
+from ament_index_python.packages import get_package_share_directory
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+
+
+def generate_launch_description():
+    simulation_dir = get_package_share_directory('nobleans-playground')
+    world = LaunchConfiguration('world')
+
+    webots = WebotsLauncher(
+        world=PathJoinSubstitution([simulation_dir, 'worlds', world])
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value='nieuwe_erven.wbt',
+            description='World file to be launched'
+        ),
+        webots,
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            )
+        )
+    ])
