56c9a4fccbe75626a0c757e2b2668715c6510c65
Tim Clephas <tim.clephas@nobleo.nl>
2021-10-19 09:49:53 +0200

    Working spawner for a sphere

--- controllers/spawner/spawner.py
+++ controllers/spawner/spawner.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """spawner controller."""
 
-# General idea here is to expose a ROS2 service that can spawn robots using the supervisor
 from controller import Supervisor
+
 
 # create the Supervisor instance.
 supervisor = Supervisor()
+
+rootNode = supervisor.getRoot();
+childrenField = rootNode.getField('children')
+childrenField.importMFNodeFromString(-1, 'Robot { children [ Shape { geometry Sphere { radius 100 } } ] }')
 
 # get the time step of the current world.
 timestep = int(supervisor.getBasicTimeStep())
@@ -21,8 +25,6 @@ while supervisor.step(timestep) != -1:
 
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
-    pass
-
     print("Something")
 
 # Enter here exit cleanup code.
